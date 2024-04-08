0d44ff1f9e7bf46a46bb15c7808674875e98ead0
Matias Davezac <8675@holbertonstudents.com>
2024-04-08 15:29:31 -0300

    Fixed header, added 0. Matthew was microwaving.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -10,10 +11,36 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i;
+	int flag;
+	int tmp;
 
+	i = 0;
+	flag = 0;
+	while (1)
+	{
+		if (array[i] > array[i + 1])
+		{
+			while (i < size - 1 && array[i] > array[i + 1])
+			{
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+				i += 1;
+			}
+			i = 0;
+			flag = 1;
+		}
 
+		if (flag)
+			flag = 0;
+		else
+			i += 1;
 
-
+		if (i == size - 1)
+			break;
+	}
 }
 
 
