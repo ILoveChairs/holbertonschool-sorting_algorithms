8bd10fa839976c492fe6ae5afd042ac796785570
Matias Davezac <8675@holbertonstudents.com>
2024-04-09 09:55:42 -0300

    Mysterius fix T0. xd.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,12 +12,9 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
-	int flag;
 	int tmp;
 
-	i = 0;
-	flag = 0;
-	while (1)
+	for (i = 0; i < size - 1; i++)
 	{
 		if (array[i] > array[i + 1])
 		{
@@ -26,20 +23,12 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[i + 1];
 				array[i + 1] = array[i];
 				array[i] = tmp;
+
 				print_array(array, size);
 				i += 1;
 			}
-			i = 0;
-			flag = 1;
+			i = -1;
 		}
-
-		if (flag)
-			flag = 0;
-		else
-			i += 1;
-
-		if (i == size - 1)
-			break;
 	}
 }
 
