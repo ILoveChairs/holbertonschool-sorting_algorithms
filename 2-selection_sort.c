79a7de5488e8f2b76505a38fefed8f58e1c42432
matthew25072005 <8855@holbertonstudents.com>
2024-04-09 18:16:57 +0000

    Betty Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,11 +23,13 @@ void swap(int *xp, int *yp)
  */
 void selection_sort(int *array, size_t size)
 {
-	for (size_t i = 0; i < size - 1; i++)
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
 	{
 		size_t min_index = i;
 
-		for (size_t j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 			if (array[j] < array[min_index])
 				min_index = j;
 
