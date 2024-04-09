0775fc487e01840c558bdf7e12047e11ba626f9d
Matias Davezac <8675@holbertonstudents.com>
2024-04-09 11:41:13 -0300

    Added T1. T-rex go rawr.

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,32 @@
 #include "sort.h"
 #include <stdlib.h>
 
+void _replace(listint_t *i, listint_t **list)
+{
+	listint_t *tmp;
+
+	while (i->prev && i->prev->n > i->n)
+	{
+		tmp = i->prev->prev;
+
+		i->prev->next = i->next;
+		i->prev->prev = i;
+		i->next = i->prev;
+		i->prev = tmp;
+
+		if (i->prev)
+			i->prev->next = i;
+		else
+			*list = i;
+
+		if (i->next->next)
+			i->next->next->prev = i->next;
+
+		print_list(*list);
+	}
+
+}
+
 /**
  * insertion_sort_list - Sorts by instertion.
  *
@@ -8,10 +34,15 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current;
 
-
-
-
+	current = *list;
+	while (current->next)
+	{
+		_replace(current, list);
+		current = current->next;
+	}
+	_replace(current, list);
 }
 
 
